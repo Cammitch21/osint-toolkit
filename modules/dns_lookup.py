e716f22e36e06b7089b12e7d596e71b1f1009b7f
Cammitch21 <50643709+Cammitch21@users.noreply.github.com>
2025-06-25 20:19:53 +1000

    Update dns_lookup.py
    Added functionality to DNS_lookup. It now has the ability to output ipv4, ipv6, mail exchange servers, name servers and canonical names of an inputted domain.

---
+++ modules/dns_lookup.py
@@ -0,0 +1,40 @@
+import dns.resolver
+
+
+# Get the different record types (ipv4, ipv6, mail exchange, name servers, canonical names)
+def get_dns_records(domain):
+    record_types = ['A', 'AAAA', 'MX', 'NS', 'TXT', 'CNAME'] 
+    results = {}
+
+# Send a dns query with the record type
+    for rtype in record_types:
+        try:
+            answers = dns.resolver.resolve(domain, rtype)
+            records = [] 
+            
+            # Store data into records
+            for rdata in answers:
+                if rtype == 'MX':
+                    records.append(str(rdata.exchange))
+                else:
+                    records.append(str(rdata))
+
+                results[rtype] = records
+        except(dns.resolver.NoAnswer, dns.resolver.NXDOMAIN, dns.resolver.Timeout, dns.resolver.NoNameservers):
+            results[rtype] = []
+        except Exception as e:
+            results[rtype] = [f"ERROR: {e}"]            
+    return results
+
+if __name__ == "__main__":
+    domain = input("Enter the Domain to lookup: ").strip()
+    records = get_dns_records(domain)
+    print(f"\nDNS records found for {domain}:\n")
+    for rtype, values in records.items():
+        print(f"{rtype}:")
+        if values:
+            for v in values:
+                print(f"  -  {v}")
+        else:
+            print("No records found.")       
+        
